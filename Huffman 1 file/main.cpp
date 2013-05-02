6ad2ae8b87273929445286810eda6e919e4c8892
Bernando Vialli <crushstorm@dyn-72-33-142-161.uwnet.wisc.edu>
2013-05-02 14:51:02 -0500

    Initial Commit

---
+++ Huffman 1 file/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Huffman 1 file
+//
+//  Created by Bernando Vialli on 5/2/13.
+//  Copyright (c) 2013 Bernando Vialli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
